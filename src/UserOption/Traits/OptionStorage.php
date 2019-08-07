e8f2937ba99bafff28b091ba5df80a4cd1ba5726
Maxim <max_stupakov@mail.ru>
2019-08-07 22:37:16 +0300

    Update OptionStorage.php

--- src/UserOption/Traits/OptionStorage.php
+++ src/UserOption/Traits/OptionStorage.php
@@ -46,7 +46,7 @@ trait OptionStorage
      * @param $key
      * @return mixed
      */
-    public function exists($userID, $key)
+    public static function exists($userID, $key)
     {
         return self::where('key', $key)->where('user_id', $userID)->exists();
     }
@@ -59,7 +59,7 @@ trait OptionStorage
      * @param null $default
      * @return |null
      */
-    public function get($userID, $key, $default = null)
+    public static function get($userID, $key, $default = null)
     {
         $option = self::query()
             ->where('user_id', $userID)
@@ -79,7 +79,7 @@ trait OptionStorage
      * @param bool $condition
      * @return \Illuminate\Support\Collection
      */
-    public function getAll($userID, $condition = false)
+    public static function getAll($userID, $condition = false)
     {
         if ($condition) {
             return self::query()
@@ -102,7 +102,7 @@ trait OptionStorage
      * @param mixed $value
      * @return void
      */
-    public function set($userID, $key, $value = null)
+    public static function set($userID, $key, $value = null)
     {
         $keys = is_array($key) ? $key : [$key => $value];
 
@@ -117,7 +117,7 @@ trait OptionStorage
      * @param string $key
      * @return bool
      */
-    public function remove($userID, $key)
+    public static function remove($userID, $key)
     {
         return (bool)self::query()
             ->where('user_id', $userID)
